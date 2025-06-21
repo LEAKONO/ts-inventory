8fee37246c355b3d58e0bbaa8a9f243c3b37503f
LEAKONO <leakonoemmanuel3@gmail.com>
2025-06-21 15:34:51 +0300

    ✨ feat: add Car and CarColor types

---
+++ src/types/car.ts
@@ -0,0 +1,12 @@
+export type CarColor='red'|'black'|'white';
+
+export type car={
+    id:number;
+    model:string;
+    color:CarColor;
+};
+export interface Car{
+id:number;
+model:string;
+color:CarColor;  
+}
